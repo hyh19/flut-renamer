bdbb5b1a38adff883ef98008aa2404235c5482ed
spadkins <spadkins@outlook.com>
2025-11-22 14:41:57 +0800

    Add firebase_remote_config dependency and update iOS Podfile.lock
    - Added `firebase_remote_config` dependency to `pubspec.yaml` for remote configuration support.
    - Updated `Podfile.lock` to include Firebase Remote Config and its dependencies.
    - Registered Firebase Remote Config plugin in `GeneratedPluginRegistrant.swift` for macOS integration.

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,6 +9,7 @@ import desktop_drop
 import device_info_plus
 import file_picker
 import firebase_core
+import firebase_remote_config
 import package_info_plus
 import path_provider_foundation
 import shared_preferences_foundation
@@ -19,6 +20,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FirebaseRemoteConfigPlugin.register(with: registry.registrar(forPlugin: "FirebaseRemoteConfigPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
